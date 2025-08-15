921810eb4b55a565c9e8efea39c5f105e4075b6e
Akbar1001 <149526516+Akbar1001@users.noreply.github.com>
2025-08-15 10:35:32 +0000

    done

--- customreact/customreact.js
+++ customreact/customreact.js
@@ -1,7 +1,22 @@
-function customRender(reactelement,conatiner)
+function customRender(reactelement,container)
 {
+    /*
     const domElement=document.createElement(reactelement.type)
     domElement.innerHTML=reactelement.children
+    domElement.setAttribute('href',reactelement.props.href);
+    domElement.setAttribute('target',reactelement.props.target);
+
+    container.appendChild(domElement);
+    */
+    const domElement=document.createElement(reactelement.type);
+    domElement.innerHTML=reactelement.children
+    for(const prop in reactelement.props){
+        if(prop=== 'children' ) continue;
+        domElement.setAttribute(prop,reactelement.props[prop]);
+    
+    }
+    container.appendChild(domElement);
+
 }
 
 const reactelement={
@@ -9,7 +24,8 @@ const reactelement={
     props:{
         href:"https://www.google.com/",
         target:"_blank"
-
-    }
+    },
+    children:'Click me to visit Google'
 }
 const container=document.querySelector("#root");
+customRender(reactelement,container);
